7b6bb9877e099eb039bea93e7527b1fdb8e29f1a
Koltyn Palmer <koltynchiaro@gmail.com>
2018-05-07 03:44:55 -0600

    goodnight

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -54,7 +54,6 @@ function orderPrompt() {
                 
                 console.log("Insufficienct quantity remaining");
 
-
                 console.log("Your ordered was placed successfully!");
           
             start();
@@ -62,3 +61,27 @@ function orderPrompt() {
         );
     });
     }
+
+
+    
+    // || ID: 5 || Product: Solmate Socks || Department: clothing || Price: $12.54 ||
+
+    // || ID: 6 || Product: 2018 Burton Step-In Bindings || Department: sports || Price: $109.39 ||
+    
+    // || ID: 7 || Product: Pair of Thieves Socks || Department: clothing || Price: $12 ||
+    
+    // || ID: 8 || Product: Sunwarrior Classic Vegan Protein 16oz || Department: grocery || Price: $35.99 ||
+    
+    // || ID: 9 || Product: HeliHuman G+Drone || Department: electronics || Price: $9999.99 ||
+
+// || ID: 10 || Product: Hermione's Time Turner || Department: enchanted || Price: $1357.91 ||
+
+// || ID: 11 || Product: Bag of Holding || Department: enchanted || Price: $2468.02 ||
+// ? What is the ID for the item you would like to purchase? 3
+// ? How many units would you like to order? 1
+// || Item ID: 3 || Product Name: Bag of Holding ||
+// Total: 2468.02
+// Your ordered was not placed successfully...
+// Insufficienct quantity remaining
+// Your ordered was placed successfully!
+// Loading all products...
